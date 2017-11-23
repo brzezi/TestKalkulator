5cfdd45667490a093630aaecd369639fb915bf60
Mateusz Brzezinski <brzezi@gmail.com>
2017-11-23 02:02:33 +0100

    Zmiana nazwy klasy

--- src/main/java/com/example/waluty/CurrencyExchangeController.java
+++ src/main/java/com/example/waluty/CurrencyExchangeController.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 import java.util.List;
 
 @Controller
-public class CurrencyExchange {
+public class CurrencyExchangeController {
     @Autowired
     NBPService nbpService;
 
